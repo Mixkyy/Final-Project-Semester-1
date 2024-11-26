35f5fe01b58b31090ae8f63e31ce00dc45026a1e
Panyakorn Lohaviboonsap <panyakorn141141@gmail.com>
2024-11-27 01:18:59 +0700

    reset

--- main.c
+++ main.c
@@ -1,40 +1,6 @@
 #include <stdio.h>
 
-int multiplication(int a, int b){
-    return a*b;
-}
-
 int main(){
-    int link = 49;
-    printf("hello baitoey\n");
-    adding(2,8);
-    divide(10,5);
-    int a = multiplication(4,9);
-    printf("%d", a);
-    printf("Hello World\n");
-    printf("linkneverleftfriend\n");
-    printf("Phureeruch\n");
-    printf("linkonfire\n");
-    printf("Linkalwaysleftfriendbecauseofheeheeheeheeheeheeheehee");
-    if (link == 49){
-        printf("Link ting puen every time everyday");
+
     }
-    something(4);
-    printf("hounghenghounghenghoungheng!!!!!!!!!!!!");
-    printf("oh ho ho ho ho ho ho ho no no no");
-}
-
-
-void adding(int a,int b){
-    printf("%d",a+b);
-}
-
-void divide(int a,int b){
-    printf("%d",a/b);
-}
-
-void something(int a){
-    int x = a * 3;
-    printf("%d", x);
-}
 
