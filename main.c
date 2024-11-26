d91022d2e1a786ac4ebf3422c56764497e393a0e
Korapin Manoprasertkoon <6421061010@cdti.ac.th>
2024-11-26 23:16:32 +0700

    scan link

--- main.c
+++ main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
 int main(){
+    int link;
+    scanf("%d", &link);
     printf("hello baitoey\n");
     printf("hello kan\n");
     printf("i sus so hard");
     printf("what");
     printf("micky");
+    
 }
